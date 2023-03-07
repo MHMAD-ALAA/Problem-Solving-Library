748b960c43cfafd3f68dd81d50f60d99a812bb24
mhmad-alaa <ma2001129@gmail.com>
2023-03-07 18:01:11 +0200

    fix binary search

--- Search/binary_search.cpp
+++ Search/binary_search.cpp
@@ -4,7 +4,7 @@ int low = 0, high = 1e9, mid;
 
 while (low <= high)
 {
-    mid = low + (high - low) / 3; 
+    mid = low + (high - low) / 2; 
     if (dowork(mid) == target) return mid; 
 
     if (dowork(mid) < target) low = mid + 1; 
