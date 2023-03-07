99da74449df7338086fa7e0cb3e54247d874386f
mhmad-alaa <ma2001129@gmail.com>
2023-03-07 17:59:30 +0200

    add binary search

---
+++ Search/binary_search.cpp
@@ -0,0 +1,14 @@
+/// @brief sorting is a most
+
+int low = 0, high = 1e9, mid; 
+
+while (low <= high)
+{
+    mid = low + (high - low) / 3; 
+    if (dowork(mid) == target) return mid; 
+
+    if (dowork(mid) < target) low = mid + 1; 
+    else high = mid - 1 ;
+}
+
+
